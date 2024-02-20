eacab4ee9b17a5383eaa910ee20b5019dfca14d5
Mahmoud <tkhattab1999@gmail.com>
2024-02-20 15:11:54 -0500

    cart slide right

--- src/script/toggleCart.js
+++ src/script/toggleCart.js
@@ -1,11 +1,17 @@
-// Function to toggle cart visibility
-
-
 export const toggleCart = () => {
     const cartContent = document.querySelector('.cart-content');
     if (cartContent) {
         const currentDisplay = window.getComputedStyle(cartContent).getPropertyValue('display');
-        cartContent.style.display = currentDisplay === 'none' ? 'block' : 'none';
+        if (currentDisplay === 'none') {
+            cartContent.style.display = 'block';
+            setTimeout(() => {
+                cartContent.style.right = '0'; // Slide in from the right
+            }, 100); // Adjust the delay as needed
+        } else {
+            cartContent.style.right = '-100%'; // Slide out to the right
+            setTimeout(() => {
+                cartContent.style.display = 'none';
+            }, 500); // Adjust the duration of the animation
+        }
     }
 }
-
